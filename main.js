28011987daff591d791b9930943cbd567f6e90f6
Gregg Tavares <github@greggman.com>
2016-11-20 05:06:08 +0900

    launch hft the new way

--- main.js
+++ main.js
@@ -5,15 +5,33 @@ const isDevMode = process.env.NODE_ENV === 'development';
 
 const happyfuntimes = require('happyfuntimes');
 const electron = require('electron');
+const querystring = require('querystring');
 const webContents = electron.webContents;
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 let gameWindow = null;
+let setupSteps = 2;
+const state = {};
 
-//electron.ipcMain.on('start', (event, arg) => {
-//  event.sender.send('start', args);
-//});
+happyfuntimes.start({
+  // port: 18679,
+  // dns: true,
+  // address: 127.0.0.1,
+  // verbose: true,
+  // debug: true,
+  // system-name: 'MyGameSystem',
+  // private-server: true,
+})
+.then((ports) => {
+  console.log("Listening on ports:", ports);
+  state.ports = ports;
+  state.port = ports[0];
+  startIfReady();
+})
+.catch((err) => {
+  console.error("error starting server:", err);
+});
 
 function createWindow() {
   gameWindow = new BrowserWindow({
@@ -21,15 +39,26 @@ function createWindow() {
     defaultEncoding: "utf8",
   });
 
-  gameWindow.loadURL(`file://${__dirname}/game.html`);
+  const settings = {
+    hftUrl: 'ws://localhost:' + state.port,
+  };
+  const settingsStr = querystring.stringify(settings);
+  gameWindow.loadURL(`file://${__dirname}/game.html?${settingsStr}`);
   if (isDevMode) {
     gameWindow.webContents.openDevTools();
   }
 }
 
+function startIfReady() {
+  --setupSteps;
+  if (setupSteps === 0) {
+    setupMenus();
+    createWindow();
+  }
+}
+
 app.on('ready', () => {
-  setupMenus();
-  createWindow();
+  startIfReady();
 });
 
 app.on('window-all-closed', () => {
